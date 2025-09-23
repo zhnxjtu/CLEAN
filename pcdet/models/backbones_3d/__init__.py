eca1f8d8d61d688ba341f9a37f757884cafa4ad9
zhnxjtu <88023788+zhnxjtu@users.noreply.github.com>
2025-09-23 12:26:59 +0800

    Add files via upload

---
+++ pcdet/models/backbones_3d/__init__.py
@@ -0,0 +1,14 @@
+from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
+from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x, VoxelBackBone8x_flexible, VoxelResBackBone8x_flexible
+from .spconv_unet import UNetV2
+
+
+__all__ = {
+    'VoxelBackBone8x': VoxelBackBone8x,
+    'UNetV2': UNetV2,
+    'PointNet2Backbone': PointNet2Backbone,
+    'PointNet2MSG': PointNet2MSG,
+    'VoxelResBackBone8x': VoxelResBackBone8x,
+    'VoxelBackBone8x_flexible': VoxelBackBone8x_flexible,
+    'VoxelResBackBone8x_flexible': VoxelResBackBone8x_flexible,
+}
